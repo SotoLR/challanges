a2c05a7119ddb84a44bb68613b3820a456fa9ee7
Roberto Soto <roberto.sotol@ext.cemex.com>
2020-09-22 17:59:44 +0200

    Properly started 008, slightly modified xorll.cpp in 006 for clarity

---
+++ DCP/008/008.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+//Make filereader.h
+//build tree from file
+//	start with static array?
+//traverse tree (what method would be faster?)
+//	try several methods, then understand why one is faster than the other
+
+
+int main(){
+
+	return 0;
+}
